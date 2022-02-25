3b2aff14da6b4d2b1cd86edf2cf856f48a932280
Kirill Morozov <moki-codes@yandex-team.ru>
2022-02-25 03:33:22 +0300

    test: markdown units
    diff test each markdown unit against expected results

---
+++ __tests__/units.js
@@ -0,0 +1,74 @@
+const {promises: { readdir, readFile, writeFile }, readdirSync, readFileSync} = require('fs');
+const {join} = require('path');
+const {compose} = require('ramda');
+
+const lexer = require('yfm-transform-fork/lexer.js');
+const extract = require('md2xliff-fork/extract.js');
+
+const {cwd} = process;
+
+const inputPath = join(cwd(), 'mock');
+const expectedPath = join(cwd(), 'expected');
+
+const logger = data => { console.log(JSON.stringify(data, null, 4)); return data; };
+
+const units = readdirSync(expectedPath);
+
+units.map(compose(xliff, skeleton, data));
+
+function data(name) {
+  const sklInputPath = join(inputPath, name, `${name}.skl.md`);
+  const xlfInputPath = join(inputPath, name, `${name}.xlf`);
+  const mdInputPath = join(inputPath, name, `${name}.md`);
+  const input = readFileSync(mdInputPath, 'utf8');
+
+  const xlfExpectedPath = join(expectedPath, name, `${name}.xlf`);
+  const xlfExpected = readFileSync(xlfExpectedPath, 'utf8');
+
+  const sklExpectedPath = join(expectedPath, name, `${name}.skl.md`);
+  const sklExpected = readFileSync(sklExpectedPath, 'utf8');
+
+  return {
+    name,
+    input,
+    expected: {
+      xlf: xlfExpected,
+      skl: sklExpected, 
+    },
+    path: {
+      input: {
+        md: mdInputPath,
+        skl: sklInputPath,
+        xlf: xlfInputPath,
+      },
+      expected: {
+        xlf: xlfExpectedPath,
+        skl: sklExpectedPath, 
+      },
+    }
+  };
+}
+
+function xliff(data) {
+  const {name, input, path: {input: {md, skl}}, expected: {xlf}} = data;
+
+  test(`${name} xliff`, () => {
+    const {xliff} = extract(input, md, skl, null, null, {lexer});
+
+    expect(xliff).toBe(xlf);
+  });
+
+  return data;
+};
+
+function skeleton(data) {
+  const {name, input, path: {input: {md, skl}}, expected} = data;
+
+  test(`${name} skeleton`, () => {
+    const {skeleton} = extract(input, md, skl, null, null, {lexer});
+
+    expect(skeleton).toBe(expected.skl);
+  });
+
+  return data;
+};
---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  moduleDirectories: ["node_modules", "."],
+};
