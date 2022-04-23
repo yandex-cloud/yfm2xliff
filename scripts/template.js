484e4fa798d2cd10daa3193240d44ae83e58d366
moki-codes <moki-codes@yandex-team.ru>
2022-04-23 21:52:12 +0300

    refactor(scripts): postinstall, template

--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,7 @@
 const render = (template, data) =>
-    template.replace(/{{(.*?)}}/g, (match) =>
-        data[match.split(/{{|}}/).filter(Boolean)[0].trim()] ?? match)
+    template.replace(
+        /{{(.*?)}}/g,
+        (match) => data[match.split(/{{|}}/).filter(Boolean)[0].trim()] ?? match,
+    );
 
-module.exports = { render };
+module.exports = {render};
