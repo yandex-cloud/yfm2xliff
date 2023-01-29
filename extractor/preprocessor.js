3a683be9945a602cf23f1ee342f599a5eecffd31
moki-codes <moki-codes@yandex-team.ru>
2023-01-29 04:49:14 +0400

    fix(extractor/preprocessor): stop trying to fix meta
    stop trying to fix incorrect yaml syntax
    
    fixing it requires more complicated parsing + we shouldn't allow users
    to misuse yaml syntax.(we don't have to support incorrect format usage)

--- extractor/preprocessor.js
+++ extractor/preprocessor.js
@@ -1,10 +1,6 @@
 const {compose, replace} = require('ramda');
 
 const strip = compose(
-    // wrap yaml values with variables in the single quotes '
-    // preventing yaml parser from failing
-    // eslint-disable-next-line
-    replace(/(?<![:-])([:-]\s)((?:\{\{).+(?=\n))/g, "$1'$2'"),
     // include, code, if, for directives
     replace(/\{%\s(include|code|if|else|endif|for|endfor)\s[^{]*%\}/g, ''),
     // eslint-disable-next-line security/detect-unsafe-regex
